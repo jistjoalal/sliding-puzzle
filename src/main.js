fd86e9ba520aadbee7547d52507d2bdf9d7666a8
jistjoalal <jistjoalal@gmail.com>
2019-07-29 17:11:55 -0700

    slidePuzzle comments

--- src/main.js
+++ src/main.js
@@ -98,9 +98,11 @@ const isSolvable = board => {
 };
 
 const slidePuzzle = board => {
+  // check solvability
   const solvable = isSolvable(board);
   if (!solvable) return null;
 
+  // init
   const solved = solve(board);
   const solvedKey = JSON.stringify(solved);
   let queue = [{ board, path: [], cost: Infinity }];
@@ -109,25 +111,28 @@ const slidePuzzle = board => {
   while (queue.length) {
     const curr = queue.shift();
 
+    // get moves from this state
     const zero = find(curr.board, 0);
     const moves = neighbors(curr.board, zero);
     for (let [y, x] of moves) {
+      // get new state
       const path = [...curr.path, curr.board[y][x]];
       const board = applyMove(curr.board, zero, [y, x]);
       const key = JSON.stringify(board);
 
+      // already visited
       if (visited.has(key)) continue;
+      visited.add(key);
 
+      // solution
       if (key == solvedKey) return path;
 
+      // score new state, add to queue
       const cost = heuristic(solved, board);
       const e = { board, path, cost };
       sortedInsert(queue, e);
-      visited.add(key);
     }
   }
-
-  return [];
 };
 
 module.exports = {
